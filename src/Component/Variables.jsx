31f09ec2ef07a7e60ea11f5a2dc695da6de54630
Yordy Padilla <yjrpadilla@gmail.com>
2023-05-25 23:34:50 -0500

    segundo commit

--- src/Component/Variables.jsx
+++ src/Component/Variables.jsx
@@ -1,8 +1,23 @@
+//todolo que se va a importar
 import React from "react"
 
+
+//---------------------------------------------
 const Variables = () => {
+    //codigo js
+    const saludo = "Hola"
+    const imagen = "https://picsum.photos/200"
+    const texto_alt = "Esto es una imagen"
+
+
+    //------------------------------------------
     return (
-        <div>Variables</div>
+        //codigo html + js
+        <div>
+            <h1>{saludo}</h1>
+            <img scr={imagen} alt={texto_alt} />
+            
+        </div>
     )
 }
 
