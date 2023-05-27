f11f3c0156bd04f67ed5d8a7d6ff7075471fa4ab
Yordy Padilla <yjrpadilla@gmail.com>
2023-05-26 21:48:16 -0500

    commit hasta cuando

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 //import Variables from './Component/Variables.jsx';
-import Contador from './Component/Contador.jsx';
+//import Contador from './Component/Contador.jsx';
 import PeticionApi from './Component/PeticionApi.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
